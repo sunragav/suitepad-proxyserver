4b8a03a583036866e79cb2d20e96d637583ae312
Sundara Raghavan <sunragav@gmail.com>
2020-01-01 10:21:23 +0100

    Removing the startedService related code and retaining only the bound service.

--- app/src/main/java/com/sunragav/suitepad/proxyserver/ProxyWebServer.kt
+++ app/src/main/java/com/sunragav/suitepad/proxyserver/ProxyWebServer.kt
@@ -1,14 +1,10 @@
 package com.sunragav.suitepad.proxyserver
 
-import android.app.PendingIntent
 import android.content.ClipData
 import android.content.Intent
 import android.os.*
-import androidx.core.app.NotificationCompat
-import app.WebServerApplication.Companion.CHANNEL_ID
 import com.sunragav.suitepad.data.Repository
 import com.sunragav.suitepad.proxyserver.BuildConfig.GET_URI_ACTION
-import com.sunragav.suitepad.proxyserver.BuildConfig.MOVE_TO_FOREGROUND_ACTION
 import dagger.android.DaggerService
 import fi.iki.elonen.NanoHTTPD.SOCKET_READ_TIMEOUT
 import java.lang.ref.WeakReference
@@ -35,10 +31,6 @@ class ProxyWebServer : DaggerService() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
             when (it.action) {
-                MOVE_TO_FOREGROUND_ACTION -> {
-                    handleMoveToForegroundAction()
-                    startActivity(intents[FILE_PROVIDER])
-                }
                 GET_URI_ACTION -> {
                     handleGetUriAction(it)
                 }
@@ -52,27 +44,9 @@ class ProxyWebServer : DaggerService() {
         if (clipData?.itemCount == 2) {
             if (isBound)
                 initHttpServerAndInformBoundClient(clipData)
-            else
-                initHttpServer(clipData)
         }
     }
 
-    private fun initHttpServer(clipData: ClipData) {
-        if (::server.isInitialized.not())
-            server = SuitePadHttpServer(
-                port = PORT,
-                dataSource = repository,
-                htmlUri = clipData.getItemAt(0).uri,
-                jsonUri = clipData.getItemAt(1).uri
-            )
-
-        if (server.isAlive.not()) {
-            server.start(SOCKET_READ_TIMEOUT, false)
-            sendBroadcast(intents["Broadcast"].also {
-                it?.putExtra("port", server.listeningPort)
-            })
-        }
-    }
 
 
     private fun initHttpServerAndInformBoundClient(clipData: ClipData) {
@@ -98,17 +72,6 @@ class ProxyWebServer : DaggerService() {
         }
     }
 
-
-    private fun handleMoveToForegroundAction() {
-        val pendingIntent = PendingIntent.getActivity(this, 0, intents[WEB_VIEW], 0)
-        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle(resources.getString(R.string.pendingIntentTitle))
-            .setSmallIcon(R.mipmap.ic_launcher_round)
-            .setContentIntent(pendingIntent)
-            .build()
-        startForeground(1234, notification)
-    }
-
     override fun onDestroy() {
         super.onDestroy()
         server.stop()
@@ -130,7 +93,6 @@ class ProxyWebServer : DaggerService() {
 
     companion object {
         private const val PORT = 8091
-        private const val WEB_VIEW = "WebView"
         private const val FILE_PROVIDER = "FileProvider"
         private const val MSG_HTTP_SERVER_STARTED = 2
         private const val NOTIFY_WEBVIEW_WHEN_HTTP_SERVER_STARTS = 1
